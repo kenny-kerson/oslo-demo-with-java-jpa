3a0b2ec80854eebc5490ea2ddfdbf5a7c236dd04
kenny-kerson <kenny.kerson@gmail.com>
2020-09-27 23:07:44 +0900

    1. develop 브랜치 신규 추가

---
+++ src/main/java/com/kenny/demo/oslodemowithjavajpa/OsloDemoWithJavaJpaApplication.java
@@ -0,0 +1,13 @@
+package com.kenny.demo.oslodemowithjavajpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OsloDemoWithJavaJpaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OsloDemoWithJavaJpaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/kenny/demo/oslodemowithjavajpa/OsloDemoWithJavaJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kenny.demo.oslodemowithjavajpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OsloDemoWithJavaJpaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
