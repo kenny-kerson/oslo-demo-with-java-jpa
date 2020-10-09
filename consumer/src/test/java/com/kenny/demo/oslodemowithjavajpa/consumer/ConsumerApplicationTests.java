d30ebed36ecd836739e72d191ac6861b8deb005c
kenny-kerson <kenny.kerson@gmail.com>
2020-10-09 12:59:07 +0900

    - broker-kafka, consumer 신규 모듈 추가 - build.gradle 의존성 정리

---
+++ consumer/src/test/java/com/kenny/demo/oslodemowithjavajpa/consumer/ConsumerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kenny.demo.oslodemowithjavajpa.consumer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConsumerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
--- infra/client-feign/src/main/java/com/kenny/demo/oslodemowithjavajpa/clientfeign/ClientFeignApplication.java
+++ infra/client-feign/src/main/java/com/kenny/demo/oslodemowithjavajpa/clientfeign/ClientFeignApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ClientFeignApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ClientFeignApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ClientFeignApplication.class, args);
+    }
 
 }
