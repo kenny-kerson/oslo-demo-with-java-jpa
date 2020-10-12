562ff62ca7a034628b103bf84004f0f466726cb8
kenny-kerson <kenny.kerson@gmail.com>
2020-10-12 23:47:29 +0900

    # biz 수신계좌정보조회 더미로직 구현

--- biz/src/main/java/com/kenny/demo/oslodemowithjavajpa/biz/account/dep/ui/DepAccountController.java
+++ biz/src/main/java/com/kenny/demo/oslodemowithjavajpa/biz/account/dep/ui/DepAccountController.java
@@ -1,7 +1,9 @@
 package com.kenny.demo.oslodemowithjavajpa.biz.account.dep.ui;
 
+import com.kenny.demo.oslodemowithjavajpa.clientfeign.dto.CommonRequest;
 import com.kenny.demo.oslodemowithjavajpa.clientfeign.dto.CommonResponse;
 import com.kenny.demo.oslodemowithjavajpa.clientfeign.dto.account.DepAccountInfo;
+import com.kenny.demo.oslodemowithjavajpa.common.code.StatusCode;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -11,9 +13,20 @@ public class DepAccountController implements DepAccountControllerSpec {
      * 수신계좌 정보조회
      */
     @Override
-    public CommonResponse<DepAccountInfo.Out> getDepAccountInfo( final String acno ) {
+    public CommonResponse<DepAccountInfo.Out> getDepAccountInfo(final CommonRequest<DepAccountInfo.In>input, final String acno ) {
         // TODO 수신계좌 정보조회 로직추가 필요!!
         return CommonResponse.<DepAccountInfo.Out>builder()
+                .guid(input.getGuid())
+                .cstno(input.getCstno())
+                .status(StatusCode.OK.getCode())
+                .dataBody(
+                        DepAccountInfo.Out.builder()
+                                .acno(input.getDataBody().getAcno())
+                                .firstDivisionCode("01")
+                                .acnoStcd("00")
+                                .acnoName("수신계좌")
+                                .build()
+                )
                 .build();
     }
 }
--- biz/src/main/java/com/kenny/demo/oslodemowithjavajpa/biz/account/dep/ui/DepAccountControllerSpec.java
+++ biz/src/main/java/com/kenny/demo/oslodemowithjavajpa/biz/account/dep/ui/DepAccountControllerSpec.java
@@ -1,12 +1,14 @@
 package com.kenny.demo.oslodemowithjavajpa.biz.account.dep.ui;
 
+import com.kenny.demo.oslodemowithjavajpa.clientfeign.dto.CommonRequest;
 import com.kenny.demo.oslodemowithjavajpa.clientfeign.dto.CommonResponse;
 import com.kenny.demo.oslodemowithjavajpa.clientfeign.dto.account.DepAccountInfo;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 
 public interface DepAccountControllerSpec {
 
     @GetMapping("/v1/biz/account/dep/info/{acno}")
-    CommonResponse<DepAccountInfo.Out> getDepAccountInfo(@PathVariable String acno);
+     CommonResponse<DepAccountInfo.Out> getDepAccountInfo(@RequestBody CommonRequest<DepAccountInfo.In> input, @PathVariable String acno);
 }
