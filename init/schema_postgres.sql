32a97082a476a5b5cf73e111053d12d61edda78a
kenny.k <kenny.kerson@gmail.com>
2021-02-11 00:12:52 +0900

    # postgresql 스키마 생성 DDL 추가

--- init/schema_postgres.sql
+++ init/schema_postgres.sql
@@ -1,3 +1,6 @@
+---------------------------------------------------------------------
+-- 1. API로그
+---------------------------------------------------------------------
 CREATE TABLE API_LOG(
     CSTNO varchar(16) NOT NULL,
     TX_DT varchar(8) NOT NULL,
@@ -11,4 +14,42 @@ CREATE TABLE API_LOG(
     HTTP_HEADER_CONTENT varchar(1000)
 );
 
-ALTER TABLE API_LOG ADD PRIMARY KEY (CSTNO, TX_DT, TX_SEQNO)
+ALTER TABLE API_LOG ADD PRIMARY KEY (CSTNO, TX_DT, TX_SEQNO);
+
+
+---------------------------------------------------------------------
+-- 2. 수신계좌기본
+---------------------------------------------------------------------
+CREATE TABLE DEPOSIT_ACCOUNT_INFO_M(
+    ACNO varchar(16) NOT NULL,
+    SYSTEM_TIMESTAMP timestamp NOT NULL default current_timestamp,
+    CSTNO varchar(16),
+    ACCO_STCD varchar(2)
+);
+
+ALTER TABLE DEPOSIT_ACCOUNT_INFO_M ADD PRIMARY KEY (ACNO);
+
+
+---------------------------------------------------------------------
+-- 3. 여신계좌기본
+---------------------------------------------------------------------
+CREATE TABLE LOAN_ACCOUNT_INFO_M(
+       ACNO varchar(16) NOT NULL,
+       SYSTEM_TIMESTAMP timestamp NOT NULL default current_timestamp,
+       CSTNO varchar(16),
+       ACCO_STCD varchar(2)
+);
+
+ALTER TABLE LOAN_ACCOUNT_INFO_M ADD PRIMARY KEY (ACNO);
+
+---------------------------------------------------------------------
+-- 4. 모임원계좌기본
+---------------------------------------------------------------------
+CREATE TABLE SHARED_ACCOUNT_INFO_M(
+    ACNO varchar(16) NOT NULL,
+    SYSTEM_TIMESTAMP timestamp NOT NULL default current_timestamp,
+    CSTNO varchar(16),
+    ACCO_STCD varchar(2)
+);
+
+ALTER TABLE SHARED_ACCOUNT_INFO_M ADD PRIMARY KEY (ACNO);
