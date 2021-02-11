98f735098a53fce6314ae084ce24bc3151364403
kenny.k <kenny.kerson@gmail.com>
2021-02-11 23:07:25 +0900

    # hub 쓰레드풀 설정 변경

--- hub/src/main/java/com/kenny/demo/oslodemowithjavajpa/hub/common/config/ThreadPoolConfig.java
+++ hub/src/main/java/com/kenny/demo/oslodemowithjavajpa/hub/common/config/ThreadPoolConfig.java
@@ -12,13 +12,18 @@ import java.util.concurrent.ThreadPoolExecutor;
 @EnableAsync
 public class ThreadPoolConfig {
 
+    private static final int CORE_POOL_SIZE = 50;
+    private static final int MAX_POOL_SIZE = 500;
+    private static final int QUEUE_CAPACITY = 1000;
+    private static final int KEEP_ALIVE_SECONDS = 60;
+
     @Bean(name = "supplyAsyncExecutor")
     public Executor supplyAsyncExecutor() {
         ThreadPoolTaskExecutor threadPoolTaskExecutor = new ThreadPoolTaskExecutor();
-        threadPoolTaskExecutor.setCorePoolSize(1);
-        threadPoolTaskExecutor.setMaxPoolSize(Integer.MAX_VALUE);
-        threadPoolTaskExecutor.setQueueCapacity(Integer.MAX_VALUE);
-        threadPoolTaskExecutor.setKeepAliveSeconds(60);
+        threadPoolTaskExecutor.setCorePoolSize(CORE_POOL_SIZE);
+        threadPoolTaskExecutor.setMaxPoolSize(MAX_POOL_SIZE);
+        threadPoolTaskExecutor.setQueueCapacity(QUEUE_CAPACITY);
+        threadPoolTaskExecutor.setKeepAliveSeconds(KEEP_ALIVE_SECONDS);
         threadPoolTaskExecutor.setThreadNamePrefix("supplyAsyncExecutor-");
         threadPoolTaskExecutor.initialize();
         return threadPoolTaskExecutor;
@@ -27,10 +32,10 @@ public class ThreadPoolConfig {
     @Bean(name = "thenAsyncExecutor")
     public Executor thenAsyncExecutor() {
         ThreadPoolTaskExecutor threadPoolTaskExecutor = new ThreadPoolTaskExecutor();
-        threadPoolTaskExecutor.setCorePoolSize(1);
-        threadPoolTaskExecutor.setMaxPoolSize(Integer.MAX_VALUE);
-        threadPoolTaskExecutor.setQueueCapacity(Integer.MAX_VALUE);
-        threadPoolTaskExecutor.setKeepAliveSeconds(60);
+        threadPoolTaskExecutor.setCorePoolSize(CORE_POOL_SIZE);
+        threadPoolTaskExecutor.setMaxPoolSize(MAX_POOL_SIZE);
+        threadPoolTaskExecutor.setQueueCapacity(QUEUE_CAPACITY);
+        threadPoolTaskExecutor.setKeepAliveSeconds(KEEP_ALIVE_SECONDS);
         threadPoolTaskExecutor.setThreadNamePrefix("thenAsyncExecutor-");
         threadPoolTaskExecutor.initialize();
         return threadPoolTaskExecutor;
