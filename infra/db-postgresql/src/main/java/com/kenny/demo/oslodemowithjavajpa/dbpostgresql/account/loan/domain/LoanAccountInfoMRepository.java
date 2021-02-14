d5826d2b8c633e9e7356dc1c3914c7ba948f9c6b
kenny.k <kenny.kerson@gmail.com>
2021-02-14 23:59:40 +0900

    # postgresql Repository 인터페이스 추가

---
+++ infra/db-postgresql/src/main/java/com/kenny/demo/oslodemowithjavajpa/dbpostgresql/account/loan/domain/LoanAccountInfoMRepository.java
@@ -0,0 +1,6 @@
+package com.kenny.demo.oslodemowithjavajpa.dbpostgresql.account.loan.domain;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface LoanAccountInfoMRepository extends JpaRepository<LoanAccountInfoM, String> {
+}
---
+++ infra/db-postgresql/src/main/java/com/kenny/demo/oslodemowithjavajpa/dbpostgresql/customer/domain/CustomerInfoMRepository.java
@@ -0,0 +1,6 @@
+package com.kenny.demo.oslodemowithjavajpa.dbpostgresql.customer.domain;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CustomerInfoMRepository extends JpaRepository<CustomerInfoM, String> {
+}
