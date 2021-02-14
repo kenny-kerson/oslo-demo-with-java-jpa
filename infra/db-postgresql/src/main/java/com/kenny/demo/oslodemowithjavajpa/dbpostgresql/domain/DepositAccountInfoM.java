c45f5fc8dada99c4afe903d046c5f6be1cd7de45
kenny.k <kenny.kerson@gmail.com>
2021-02-14 23:18:18 +0900

    # biz ap, db-postgresql yml 설정 적용

--- infra/db-postgresql/src/main/java/com/kenny/demo/oslodemowithjavajpa/dbpostgresql/domain/DepositAccountInfoM.java
+++ infra/db-postgresql/src/main/java/com/kenny/demo/oslodemowithjavajpa/dbpostgresql/domain/DepositAccountInfoM.java
@@ -1,5 +1,24 @@
 package com.kenny.demo.oslodemowithjavajpa.dbpostgresql.domain;
 
+import lombok.*;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import java.math.BigDecimal;
+
+@Entity
+@Getter
+@NoArgsConstructor
+@Builder
+@ToString
 public class DepositAccountInfoM {
 
+    @Id
+    private String accountNo;
+    private String customerNo;
+    private String accountStatus;
+    private BigDecimal balance;
+    private String goodsCode;
+    private String goodsDetailCode;
 }
--- infra/db-postgresql/src/main/java/com/kenny/demo/oslodemowithjavajpa/dbpostgresql/domain/SharedAccountInfoM.java
+++ infra/db-postgresql/src/main/java/com/kenny/demo/oslodemowithjavajpa/dbpostgresql/domain/SharedAccountInfoM.java
@@ -1,4 +1,26 @@
 package com.kenny.demo.oslodemowithjavajpa.dbpostgresql.domain;
 
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import java.math.BigDecimal;
+
+@Entity
+@Getter
+@NoArgsConstructor
+@Builder
+@ToString
 public class SharedAccountInfoM {
+
+    @Id
+    private String accountNo;
+    private String customerNo;
+    private String accountStatus;
+    private BigDecimal balance;
+    private String goodsCode;
+    private String goodsDetailCode;
 }
